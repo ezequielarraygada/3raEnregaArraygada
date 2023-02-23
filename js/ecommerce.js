1101eb20510e075ee97014b2581fecafa488d74d
ezequielarraygada <116087261+ezequielarraygada@users.noreply.github.com>
2023-02-23 18:46:24 -0300

    PF

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -1,36 +1,29 @@
 // // Capturas DOM
-// const form = document.getElementById("IngresoProductos")
-
-// //Captura LocaleStorage
-
-// const ListaProductos = JSON.parse(localStorage.getItem(Lista))
+const form = document.getElementById("IngresoProductos")
 
 // // Eventos
-// form.addEventListener("submit", (e)=>{
-//     e.preventDefault()
-//     AgregarProducto(ListaProductos)
-//     console.log("Boton Agregar Funciona")
-//   })
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    AgregarProducto(Lista)
+  })
 
-//   //Funciones
-//   function AgregarProducto(array) {
-//     let IProducto = document.getElementById("ProductoI")
-//     let IClase = document.getElementById("ClaseI")
-//     let ITipo = document.getElementById("TipoI")
-//     let IDetalle = document.getElementById("DetalleI")
-//     let IPrecio = document.getElementById("PrecioI")
+   //Funciones
+   function AgregarProducto(array) {
+     let IProducto = document.getElementById("ProductoI")
+     let IClase = document.getElementById("ClaseI")
+     let ITipo = document.getElementById("TipoI")
+     let IDetalle = document.getElementById("DetalleI")
+     let IPrecio = document.getElementById("PrecioI")
   
-//     const NuevoProducto = new Producto(ListaProductos.length + 1, IProducto.value,
-//       IClase.value, ITipo.value, IDetalle.value, parseInt(IPrecio.value), "nodisponible.jpg")
-//     console.log(NuevoProducto)
-  
-//     array.push(NuevoProducto)
-//     console.log(array)
-  
-//     localStorage.setItem("Lista", JSON.stringify(array))
-//     MostrarCatalogo(array)
-  
-//     IProducto.value = ""
-//     IDetalle.value = ""
-//     IPrecio.value = ""
-//   }
+     const NuevoProducto = new Producto(Lista.length + 1, IProducto.value,
+       IClase.value, ITipo.value, IDetalle.value, parseInt(IPrecio.value), "nodisponible.jpg")
+     Lista.push(NuevoProducto)
+     localStorage.setItem("Lista", JSON.stringify(array))
+     swal({
+      title: "¡Producto agregado!",
+      text: `Producto:${NuevoProducto.producto} ha sido agregado a la lista de productos`,
+      icon: "success",
+      button: "Ok",
+    });
+     form.reset()
+   }
